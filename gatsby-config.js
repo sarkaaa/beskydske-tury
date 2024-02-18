581886c83197729aadf2ba1e40bcca92faba2ca1
Sarka Chwastkova <sarka.chwastkova@kiwi.com>
2024-02-18 15:27:26 +0100

    removed env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,9 +58,7 @@ module.exports = {
       resolve: "gatsby-source-strapi",
       options: {
         apiURL:  process.env.API_URL || "http://localhost:1337",
-        // apiURL:  "http://localhost:1337",
         accessToken: process.env.API_TOKEN,
-        // accessToken: "6bd52f90dc03e472d3fe5c3e428cac71dd44c44809d52e6df482f43bb2f85e074b5d1b96f1c3f4862bb0cc872a0fe4cd459b33aee01a88203d61d6766f46664e641bb270c8a9bdc21c0f543d0825010e11accaaa97a6668736558c1a55b5301e01fa918d5c1ba6889b5d711835f6ba487ea63504b979b82f358b0564cac4edea",
         collectionTypes: ["user", {
           singularName: "trail",
           queryParams: {
